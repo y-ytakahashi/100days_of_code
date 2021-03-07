0caf532e747cd617d60cb8d19e879f246f25968c
y-ytakahashi <novsleep@outlook.jp>
2021-03-07 14:37:31 +0900

    3-2 スネークを真っ直ぐ動かす

--- snakegame/snake-game/src/utils/index.ts
+++ snakegame/snake-game/src/utils/index.ts
@@ -1,5 +1,5 @@
 // 35 x 35  の二次元配列を生成する
-export const initFields = (fieldSize:number) => {
+export const initFields = (fieldSize:number, initialPosition:any) => {
   const fields = [];
   for (let i = 0; i < fieldSize; i++) {
     const cols = new Array(fieldSize).fill("");
@@ -7,5 +7,6 @@ export const initFields = (fieldSize:number) => {
     console.log({cols},{fields})
   }
   //fieldSize の二次元配列を生成（ fieldSize x 列 fieldSize x 行）
+  fields[initialPosition.y][initialPosition.x] = 'snake';
   return fields
 };
