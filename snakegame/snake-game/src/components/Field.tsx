aeae1909e0986649638bf02592bba2d06cd8778c
y-ytakahashi <novsleep@outlook.jp>
2021-03-07 09:31:53 +0900

    1-1 スネークゲームのコンポーネントを切り出す

---
+++ snakegame/snake-game/src/components/Field.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Field = () => {
+  return (
+    <div>
+      <p>Field</p>
+    </div>
+  )
+}
+
+export default Field
---
+++ snakegame/snake-game/src/components/ManipulationPanel.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ManipulationPanel = () => {
+  return (
+    <div>
+      <div className="mainipulation-panel"></div>
+      <button>←</button>
+      <button>↑</button>
+      <button>↓</button>
+      <button>→</button>
+    </div>
+  )
+}
+
+export default ManipulationPanel
