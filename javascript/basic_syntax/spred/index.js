5b5e5b14dc293218b1009f3ba066509d3476c3c7
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2021-03-20 22:51:10 +0900

    ショートカット演算子

--- javascript/basic_syntax/spred/index.js
+++ javascript/basic_syntax/spred/index.js
@@ -7,4 +7,16 @@ let { title: name, publish: company } = book;
 console.log(name);
 
 
+// ショートカット演算子
+
+let x = 1;
+//if文を使った場合
+if( x === 1){console.log('this true' )}
+
+//ショートカット演算子を使った場合
+x === 1 && console.log('this true')
+
+
+
+
 
