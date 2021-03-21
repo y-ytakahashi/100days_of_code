c0b36741eb824e7570a43d375a5342713931d9e0
y-ytakahashi <ty.ytakahashi.work@gmail.com>
2021-03-21 12:21:24 +0900

    mapメソッドの実装復習 移送速度と特定の情報の抜き出し

--- javascript/basic_syntax/map/index.js
+++ javascript/basic_syntax/map/index.js
@@ -1,11 +1,11 @@
-// 復習
+;// 復習
 
 const numbers = [1,2,3];
 let doubleNumbers = [];
 
 
+// アロー関数を利用して map メソッドを実装
 doubleNumbers = numbers.map((item) => item * 2)
-
 console.log({doubleNumbers})
 
 // 無名関数を利用して map メソッドを実装
@@ -15,3 +15,30 @@ let numberFunction = numbers.map(function(number){
 
 console.log({numberFunction});
 
+// Object から特定の要素を取得する
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+var heights;
+heights = images.map(image => image.height);
+console.log({heights})
+
+// 移動速度を求める
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds;
+speeds = trips.map(item => item.distance / item.time);
+console.log(speeds);
+
+// 渡したプロパティの値だけを返却する関数を実装
+
+var colorObjects = [{ color: '赤' }, { color: '青' }, { color: '黄色' }];
+var colorNames = colorObjects.map(item => item['color']);
+console.log({colorNames})
